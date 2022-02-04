bbd4631343be1698f68b4837ec4982c8c286aec1
Jhay24 <46474160+Jhay24@users.noreply.github.com>
2022-02-05 00:56:49 +0800

    Update config.php

--- config.php
+++ config.php
@@ -8,6 +8,24 @@ define('DB_NAME', 'my_db');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+/* Heroku remote server */
+$i++;
+$cfg["Servers"][$i]["host"] = "localhost"; //provide hostname
+$cfg["Servers"][$i]["user"] = "root"; //user name for your remote server
+$cfg["Servers"][$i]["password"] = " "; //password
+$cfg["Servers"][$i]["auth_type"] = "config"; // keep it as config
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
  
 // Check connection
 if($link === false){
